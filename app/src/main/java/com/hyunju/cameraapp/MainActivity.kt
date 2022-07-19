1a98c36d01e2def51eeb975aca9097ee4f1c992a
hyunju <dear.jjwim@gmail.com>
2022-07-19 23:32:24 +0900

    카메라 촬영 이미지 저장

--- app/src/main/java/com/hyunju/cameraapp/MainActivity.kt
+++ app/src/main/java/com/hyunju/cameraapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.hardware.display.DisplayManager
+import android.media.MediaScannerConnection
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -16,7 +18,12 @@ import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.hyunju.cameraapp.databinding.ActivityMainBinding
+import com.hyunju.cameraapp.util.PathUtil
+import java.io.File
+import java.io.FileNotFoundException
 import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -24,7 +31,11 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // ExecutorService: 병렬 작업 시 여러 개의 작업을 효율적으로 처리하기 위해 제공되는 JAVA 라이브러리
     private lateinit var cameraExecutor: ExecutorService
+
+    // 이 컨텍스트와 연결된 메인 스레드에서 대기열에 있는 작업을 실행할 Executor를 반환합니다.
+    // 이것은 응용 프로그램 구성 요소(활동, 서비스 등)에 대한 호출을 전달하는 데 사용되는 스레드입니다.
     private val cameraMainExecutor by lazy { ContextCompat.getMainExecutor(this) }
     private val cameraProviderFuture by lazy { ProcessCameraProvider.getInstance(this) }
 
@@ -37,6 +48,8 @@ class MainActivity : AppCompatActivity() {
     private var displayId: Int = -1
 
     private var camera: Camera? = null
+
+    private var isCapturing: Boolean = false
 
     private val displayListener = object : DisplayManager.DisplayListener {
         override fun onDisplayAdded(displayId: Int) = Unit
@@ -83,6 +96,8 @@ class MainActivity : AppCompatActivity() {
         val cameraSelector =
             CameraSelector.Builder().requireLensFacing(LENS_FACING).build() // 카메라 설정(후면)
 
+        // 매개변수 중에 Executor가 따로 설정되어있지 않으면 Main Thread에서 SurfaceProvider를 제공한다.
+        // 만약 null 값으로 Provider를 제거하면 카메라는 Preview 객체에서 이미지를 만드는 것을 멈춘다.
         cameraProviderFuture.addListener({
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
             val preview = Preview.Builder().apply {
@@ -104,10 +119,69 @@ class MainActivity : AppCompatActivity() {
                     this@MainActivity, cameraSelector, preview, imageCapture
                 )
                 preview.setSurfaceProvider(viewFinder.surfaceProvider)
+                bindCaptureListener()
             } catch (e: Exception) {
                 e.printStackTrace()
             }
         }, cameraExecutor)
+    }
+
+    private fun bindCaptureListener() = with(binding) {
+        captureButton.setOnClickListener {
+            if (isCapturing.not()) {
+                isCapturing = true
+                captureCamera()
+            }
+        }
+    }
+
+    private fun updateSavedImageContent() {
+        contentUri?.let {
+            isCapturing = try {
+                val file = File(PathUtil.getPath(this, it) ?: throw FileNotFoundException())
+                MediaScannerConnection.scanFile(
+                    this,
+                    arrayOf(file.path),
+                    arrayOf("image/jpeg"),
+                    null
+                )
+                false
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this, "파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                false
+            }
+        }
+    }
+
+    private var contentUri: Uri? = null
+
+    private fun captureCamera() {
+        if (::imageCapture.isInitialized.not()) return
+        val photoFile = File(
+            PathUtil.getOutputDirectory(this),
+            SimpleDateFormat(
+                FILENAME_FORMAT, Locale.KOREA
+            ).format(System.currentTimeMillis()) + ".jpg"
+        )
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(
+            outputOptions,
+            cameraExecutor,
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                    val savedUri = outputFileResults.savedUri ?: Uri.fromFile(photoFile)
+                    contentUri = savedUri
+                    updateSavedImageContent()
+                }
+
+                override fun onError(e: ImageCaptureException) {
+                    e.printStackTrace()
+                    isCapturing = false
+                }
+
+            })
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -123,9 +197,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-        private val LENS_FACING: Int = CameraSelector.LENS_FACING_BACK
+        private const val LENS_FACING: Int = CameraSelector.LENS_FACING_BACK
     }
 
 }
